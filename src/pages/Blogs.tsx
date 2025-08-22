46c7d7d3c0cba53d3168135846aaf67095226380
saad-mahmoood <saad.mhmoood@gmail.com>
2025-08-22 12:21:07 +0500

    correct content

--- src/pages/Blogs.tsx
+++ src/pages/Blogs.tsx
@@ -291,17 +291,7 @@ const Blogs: React.FC = () => {
         <div className="bg-white dark:bg-gray-800 shadow-md sticky top-16 z-40 border-t border-gray-200 dark:border-gray-700">
           <div className="container mx-auto px-6 py-6">
             <div className="flex flex-col lg:flex-row gap-4 items-center justify-between">
-              {/* Search */}
-              <div className="relative flex-1 max-w-md">
-                <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-5 h-5" />
-                <input
-                  type="text"
-                  placeholder="Search articles..."
-                  value={searchQuery}
-                  onChange={(e) => setSearchQuery(e.target.value)}
-                  className="w-full pl-10 pr-4 py-2 border border-gray-300 dark:border-gray-600 rounded-lg focus:ring-2 focus:ring-navy-500 dark:focus:ring-yellow-500 focus:border-transparent bg-white dark:bg-gray-700 text-gray-900 dark:text-white"
-                />
-              </div>
+       
 
               {/* Filters */}
               <div className="flex flex-wrap gap-3 items-center">
